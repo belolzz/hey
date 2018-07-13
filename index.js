979d41d899671775e59304eb6369abbc8e2f410a
belolzz <41213596+belolzz@users.noreply.github.com>
2018-07-14 06:22:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const client = new Discord.Client();
 
 // Set the prefix
 let prefix = "!";
+client,on('ready',() => {
+  consle.log('I am online');
+});
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
